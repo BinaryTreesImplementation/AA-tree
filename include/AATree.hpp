cf4f0b5b56a727d5f6afdbf1fa34c013787d3d35
Bozey98 <dmusatov98@gmail.com>
2017-12-19 00:17:33 +0300

    Update AATree.hpp

--- include/AATree.hpp
+++ include/AATree.hpp
@@ -3,7 +3,7 @@
 class Counter
 {
 protected:
-	size_t& Count() { static size_t counter = -1; return counter; }
+	size_t& Count() { static size_t counter = 0; return counter; }
 	
 public:
 	Counter() { ++Count(); }
